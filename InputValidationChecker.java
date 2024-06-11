5e330b7e7e505cbf105191dccd8aa5dad9dbd085
srikanth-fernando <srikanthfernando3@gmail.com>
2024-06-11 13:04:07 +0530

    Moified the InputValidationChecker.java

--- InputValidationChecker.java
+++ InputValidationChecker.java
@@ -6,30 +6,41 @@
  * Created - 11/06/2024                                                 *
  * Last Modified - 11/06/2024                                           *
  ***********************************************************************/
+import java.util.*;
+
 public class InputValidationChecker {
     /***********************************************************************************************************************************
      * Method - IntegerValidationChecker                                                                                               *
-     * Import -                                                                                                                        *
-     * Export -                                                                                                                        *
+     * Import - pPromptToUser (String)                                                                                                 *
+     * Export - number (Integer)                                                                                                       *
      * Description - This function check the user input is valid or not. If it is valid it will return the value or else it loop until *
      *                  the enter valid integer                                                                                        *
      ***********************************************************************************************************************************/
     public static int IntegerValidationChecker(String pPromptToUser) {
         Scanner userInput = new Scanner(System.in);
-        int number;
+        int number = 0;
+        boolean isNotValid = true;
 
-        while (!userInput.hasNextInt()) {
+        do {
             try {
                 System.out.print(pPromptToUser);
                 number = userInput.nextInt();
 
-            } catch (InputMisMactch e) {
-                System.out.println("Please enter a valid number");
+                isNotValid = false;
+
+            } catch (InputMismatchException e) {
+                System.out.println("\nERROR: Please enter a number.\n");
+                userInput.next(); // Clear the user input
+
+            } catch (NoSuchElementException e) {
+                System.out.println("\nERROR: Do not leave the field empty.\n");
                 userInput.next(); // Clear the user input
 
             }
 
-        }
+        } while (isNotValid);
+
+        return number;
 
     }
 
