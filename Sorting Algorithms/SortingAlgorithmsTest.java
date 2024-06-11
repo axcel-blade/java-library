df715260051dc48271786116aa3a884c13760df4
srikanth-fernando <srikanthfernando3@gmail.com>
2024-06-11 18:00:50 +0530

    Modified SortingAlgorithmsTest.java

--- Sorting Algorithms/SortingAlgorithmsTest.java
+++ Sorting Algorithms/SortingAlgorithmsTest.java
@@ -1,14 +1,35 @@
 import java.util.*;
 import java.io.*;
 
-class SortingAlgorithms {
+class SortingAlgorithmsTest {
     public static void main(String[] args) {
         SortingAlgorithms sortingAlgorithms = new SortingAlgorithms();
         int[] numArray = {5, 2, 8, 1, 9};
         int[] sortedArray = {1, 2, 5, 8, 9};
 
-        int[] tempArray = sortingAlgorithms.bubbleSort(numArray);
-        assert Arrays.equals(sortedArray, tempArray) : "Test Failed";
+        bubbleSortTest(sortingAlgorithms, numArray, sortedArray);
+        selectionSortTest(sortingAlgorithms, numArray, sortedArray);
+        insertionSortTest(sortingAlgorithms, numArray, sortedArray);
+
+    }
+
+    public static void bubbleSortTest(SortingAlgorithms pSortingAlgorithms, int[] pNumArray, int[] pSortedArray) {
+        int[] tempArray = pSortingAlgorithms.bubbleSort(pNumArray);
+        assert Arrays.equals(pSortedArray, tempArray) : "Test Failed";
+        System.out.println("Test Passed");
+
+    }
+
+    public static void selectionSortTest(SortingAlgorithms pSortingAlgorithms, int[] pNumArray, int[] pSortedArray) {
+        int[] tempArray = pSortingAlgorithms.bubbleSort(pNumArray);
+        assert Arrays.equals(pSortedArray, tempArray) : "Test Failed";
+        System.out.println("Test Passed");
+
+    }
+
+    public static void insertionSortTest(SortingAlgorithms pSortingAlgorithms, int[] pNumArray, int[] pSortedArray) {
+        int[] tempArray = pSortingAlgorithms.bubbleSort(pNumArray);
+        assert Arrays.equals(pSortedArray, tempArray) : "Test Failed";
         System.out.println("Test Passed");
 
     }
