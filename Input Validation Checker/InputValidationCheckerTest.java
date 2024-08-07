3d0933d59499d10103cec05cde82b4881204b60b
srikanth-fernando <srikanthfernando3@gmail.com>
2024-08-07 09:32:08 +0530

    Modified the code

--- Input Validation Checker/InputValidationCheckerTest.java
+++ Input Validation Checker/InputValidationCheckerTest.java
@@ -4,12 +4,24 @@ public class InputValidationCheckerTest {
     public static void main(String[] args) {
         InputValidationChecker validationChecker = new InputValidationChecker();
 
+        emptyInteger(validationChecker);
+        System.out.println("Test Passed");
+
         isValidInteger(validationChecker);
         System.out.println("Test Passed");
 
         isNotValidInteger(validationChecker);
         System.out.println("Test Passed");
 
+    }
+
+    public static void emptyInteger(InputValidationChecker pValidationChecker) {
+        String userInput = "";
+
+        InputStream inputStream = new ByteArrayInputStream(userInput.getBytes());
+        System.setIn(inputStream);
+
+        assert 10 == pValidationChecker.integerValidationChecker("Enter a number: ") : "Test Failed.";
     }
 
     public static void isValidInteger(InputValidationChecker pValidationChecker) {
